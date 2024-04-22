55fd0b571a2bce2585cd7e00eb3d4024cf2996e8
malaika75 <malaika57680@gmail.com>
2024-04-22 19:08:19 +0500

    currency converter

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let currency = {
+    USD: 1,
+    EUR: 0.92,
+    GBP: 0.79,
+    INR: 83,
+    PKR: 280,
+    SAR: 3.75,
+};
+const answer = await inquirer.prompt([
+    {
+        name: "fromcurrency",
+        type: "list",
+        message: "select the source currency?",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "SAR"]
+    },
+    {
+        name: "tocurrency",
+        type: "list",
+        message: "select the target currency?",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "SAR"],
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "Enter amount"
+    },
+]);
+let from = currency[answer.fromcurrency];
+let to = currency[answer.tocurrency];
+let selectAmount = answer.amount;
+let total = selectAmount / from;
+let convertedAmount = total * to;
+console.log(convertedAmount);
